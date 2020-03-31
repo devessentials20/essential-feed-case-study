18fcc395e4c97c3d892570639831149166ebe03c
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-03-31 13:38:03 +0530

    Create EssentialFeed Framework

---
+++ EssentialFeed/EssentialFeed/EssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by panditpakhurde on 31/03/20.
+//  Copyright © 2020 Quikr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
