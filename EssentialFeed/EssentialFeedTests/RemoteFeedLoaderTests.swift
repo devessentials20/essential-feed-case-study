1fc3e88757bff2f30cc90c359623cd1b2ed971e5
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-03-31 16:58:14 +0530

    Request data from URL on 'load'

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,41 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by panditpakhurde on 31/03/20.
+//  Copyright © 2020 Quikr. All rights reserved.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string:"https://a-image.url.com")
+    }
+}
+
+class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        let _ = RemoteFeedLoader()
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        _ = sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
+
+}
