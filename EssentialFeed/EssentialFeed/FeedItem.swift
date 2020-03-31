aa02e17373098b3a8aebffcce659097e81d2d3c7
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-03-31 13:39:01 +0530

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by panditpakhurde on 31/03/20.
+//  Copyright © 2020 Quikr. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
