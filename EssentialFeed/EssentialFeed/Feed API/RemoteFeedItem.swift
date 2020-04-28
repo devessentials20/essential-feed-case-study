06221374f833419373b70f9708c095ed95575ff5
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-04-28 17:51:18 +0530

    Move 'RemoteFeedItem' to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by panditpakhurde on 28/04/20.
+//  Copyright © 2020 Quikr. All rights reserved.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
+
