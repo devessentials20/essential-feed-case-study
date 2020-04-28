69fd1cd7bfb43b0d401235e3b75a32a37fff2943
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-04-28 17:52:37 +0530

    Move 'LocalFeedItem' to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,26 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by panditpakhurde on 28/04/20.
+//  Copyright © 2020 Quikr. All rights reserved.
+//
+
+import Foundation
+
+public struct LocalFeedItem : Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID,
+                description: String?,
+                location: String?,
+                imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
