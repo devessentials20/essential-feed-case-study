68c94d4098b9694c2054980ecf30fad7b4163691
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-05-09 06:33:31 +0530

    Validate cache command does not delete less than seven days old cache.

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -31,6 +31,18 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         store.completeRetrievalWithEmptyCache()
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
+    
+    func test_validateCache_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDayOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        sut.validateCache()
+        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDayOldTimestamp)
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+
 
 
     
@@ -48,6 +60,32 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         return NSError(domain: "Any Error", code: 0)
     }
 
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "Any", location: "Any", url: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let items = [uniqueImage(),uniqueImage() ]
+        let localFeedItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (items, localFeedItems)
+    }
+
 
 
 }
+
+private extension Date {
+    
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
+}
+
