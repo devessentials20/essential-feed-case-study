90760c938ef7581cffff24da558f935088074409
panditpakhurde <devessentials20@gmail.com>
2020-07-25 14:59:15 +0530

    Rendered loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Pakhurde Pandit on 25/07/20.
+//  Copyright © 2020 Quikr. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+
+}
